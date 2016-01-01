95540952a9e4cbd08ab0efb2e282fda4b4a3aa93
thegands <thegands@gmail.com>
2016-01-01 22:55:28 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+def turn_count(board)
+  count = 0
+  board.each do |token|
+    token == "X" || token == "O" ? count += 1
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board).odd? ? "O" : "X"
+end
