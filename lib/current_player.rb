67d81ca08e43a13ef1c34b7db0658ab659f37a6f
swdavies7 <swdavies7@gmail.com>
2016-02-07 23:17:03 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  i = 0
+  board.each do |char|
+    if char == "X"
+      i+=1
+    elsif char == "O"
+      i+=1
+    end
+  end
+  return i
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
